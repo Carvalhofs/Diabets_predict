05e440e017b9cf447fa250410903b93195835403
Felipe Carvalho (Carvalho.fs) <86269606+Carvalhofs@users.noreply.github.com>
2022-09-29 22:23:07 -0300

    out if

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ st.title("Diabets prediction")
 with st.form("features"):
     numero_gravidez = st.number_input("Quantas vezes você já engravidou?", min_value = 0, value = 0)
     enviar = st.form_submit_button("Enviar")
-    if enviar:
-        st.write(numero_gravidez)
+if enviar:
+    st.write(numero_gravidez)

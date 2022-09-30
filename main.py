2007fa0cc0a5f2c7fe0ce6079dbe3e992ec78877
Felipe Carvalho (Carvalho.fs) <86269606+Carvalhofs@users.noreply.github.com>
2022-09-29 22:22:28 -0300

    numb grav

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ import streamlit as st
 st.title("Diabets prediction")
 
 with st.form("features"):
-    numero_gravidez = st.number_input("Quantas vezes você já engravidou? (Caso nenhuma colocar zero '0')", min_value = 0, value = 0)
-    st.form_submit_button("Enviar")
+    numero_gravidez = st.number_input("Quantas vezes você já engravidou?", min_value = 0, value = 0)
+    enviar = st.form_submit_button("Enviar")
+    if enviar:
+        st.write(numero_gravidez)

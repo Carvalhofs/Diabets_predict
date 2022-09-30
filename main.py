da17ffaece050bd2cc4482befc19bfde6c4f8244
Felipe Carvalho (Carvalho.fs) <86269606+Carvalhofs@users.noreply.github.com>
2022-09-29 22:19:45 -0300

    Criação de formulário para gravidez

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
 st.title("Diabets prediction")
+
+with st.form():
+    numero_gravidez = st.number_input("Quantas vezes você já engravidou? (Caso nenhuma colocar zero '0')", min_value = 0, value = 0)
+    st.form_submit_button("Enviar")

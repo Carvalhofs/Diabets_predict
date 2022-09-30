97ebc349fa6135292619826f17bc831516a2e32e
Felipe Carvalho (Carvalho.fs) <86269606+Carvalhofs@users.noreply.github.com>
2022-09-29 22:20:35 -0300

    form preg

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.title("Diabets prediction")
 
-with st.form():
+with st.form("features"):
     numero_gravidez = st.number_input("Quantas vezes você já engravidou? (Caso nenhuma colocar zero '0')", min_value = 0, value = 0)
     st.form_submit_button("Enviar")

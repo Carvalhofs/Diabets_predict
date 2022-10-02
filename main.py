f5981cff02f4e0fc4d1e35fc8a8c483d79be2f4c
Felipe Carvalho (Carvalho.fs) <86269606+Carvalhofs@users.noreply.github.com>
2022-10-02 15:46:12 -0300

    predict

--- main.py
+++ main.py
@@ -8,22 +8,17 @@ pickle_in = open('classifier.pkl', 'rb')
 classifier = pickle.load(pickle_in)
 st.title("Diabets prediction")
 
-with st.form("imc"):
+with st.form("features"):
     altura = st.number_input ("Qual a sua altura em metros?")
     peso = st.number_input ("Qual o seu peso em Quilos (Kg)?")
-    enviar = st.form_submit_button("Enviar")
-    if enviar:
-        ind_mass_corp = (peso/(altura*altura))
-        st.write(f'Seu índice de massa corpórea (IMC) é {ind_mass_corp}')
-with st.form("features"):
     numero_gravidez = st.number_input("Quantas vezes você já engravidou?", min_value = 0, value = 0)
-    glicose = st.number_input ("Quantos ppm de Glicose foi reportado no último exame de sangue?", value = 0)
+    glicose = st.number_input ("Quantos mg/dL de Glicose foi reportado no último exame de sangue?", value = 0)
     pressao_sangue = st.number_input ("Qual a sua pressão sanguínea no último exame realizado?", value = 0)  
     espessura_pele = st.number_input ("Qual a espessura da dobra subcutanea tricipital em milímetros? (medida realizada com adipometro na regiao do tríceps)", value = 0)
     insulina = st.number_input ("Quantos µIU/mL de Insulina foi reportado no último exame de sangue?", value = 0)
     fat_pred_diab = st.number_input ("Qual o seu fator de predisposição à diabetes? (baseado em histórico familiar)", value = 0.0, format = "%f")
     idade = st.number_input ("Qual a sua idade nesse momento?", value = 0)
-    
+    ind_mass_corp = (peso/(altura*altura))
     enviar = st.form_submit_button("Enviar")
 
 def prediction(ind_mass_corp, numero_gravidez, glicose, pressao_sangue,espessura_pele,insulina,fat_pred_diab,idade, model = classifier):  
@@ -32,4 +27,8 @@ def prediction(ind_mass_corp, numero_gravidez, glicose, pressao_sangue,espessura
         [[ind_mass_corp, numero_gravidez, glicose, pressao_sangue,espessura_pele,insulina,fat_pred_diab,idade]])
     print(prediction)
     return prediction
+if enviar:
+    st.write('Seu índice de massa corpórea (IMC) é {:.2f}'.format(ind_mass_corp))
+    st.write(prediction(ind_mass_corp, numero_gravidez, glicose, pressao_sangue,espessura_pele,insulina,fat_pred_diab,idade))
+    
 

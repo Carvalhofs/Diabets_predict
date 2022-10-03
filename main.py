87d7871f82ec8b245976820a0ae2444a70d189f0
Felipe Carvalho (Carvalho.fs) <86269606+Carvalhofs@users.noreply.github.com>
2022-10-03 18:53:51 -0300

    df:

--- main.py
+++ main.py
@@ -25,8 +25,8 @@ with st.form("features"):
 def prediction(ind_mass_corp, numero_gravidez, glicose, pressao_sangue,espessura_pele,insulina,fat_pred_diab,idade, model = classifier):  
    
     prediction = model.predict(
-        pd.DataFrame({'Pregnancies':numero_gravidez, 'Glucose':glicose, 'BloodPressure':pressao_sangue, 'SkinThickness':espessura_pele, 'Insulin':insulina,
-       'BMI':ind_mass_corp, 'DiabetesPedigreeFunction':fat_pred_diab, 'Age':idade}))
+        pd.DataFrame({'Pregnancies':[numero_gravidez], 'Glucose':[glicose], 'BloodPressure':[pressao_sangue], 'SkinThickness':[espessura_pele], 'Insulin':[insulina],
+       'BMI':[ind_mass_corp], 'DiabetesPedigreeFunction':[fat_pred_diab], 'Age':[idade]}))
     print(prediction)
     return prediction
 if enviar:

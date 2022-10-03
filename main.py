fd51eabf191e31d6b81566006c5d84ed3f65eaa9
Felipe Carvalho (Carvalho.fs) <86269606+Carvalhofs@users.noreply.github.com>
2022-10-03 00:09:47 -0300

    .,.,

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ with st.form("features"):
     peso = st.number_input ("Qual o seu peso em Quilos (Kg)?")
     numero_gravidez = st.number_input("Quantas vezes você já engravidou?", min_value = 0, value = 0)
     glicose = st.number_input ("Quantos mg/dL de Glicose foi reportado no último exame de sangue?", value = 0)
-    pressao_sangue = st.number_input ("Qual a sua pressão sanguínea no último exame realizado?", value = 0)  
+    pressao_sangue = st.number_input ("Qual a sua pressão sanguínea diastólica em mmHg no último exame realizado?", value = 0)  
     espessura_pele = st.number_input ("Qual a espessura da dobra subcutanea tricipital em milímetros? (medida realizada com adipometro na regiao do tríceps)", value = 0)
     insulina = st.number_input ("Quantos µIU/mL de Insulina foi reportado no último exame de sangue?", value = 0)
     fat_pred_diab = st.number_input ("Qual o seu fator de predisposição à diabetes? (baseado em histórico familiar)", value = 0.0, format = "%f")

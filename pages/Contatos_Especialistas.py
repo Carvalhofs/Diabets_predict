715c75278ca2139ff95a0ec84062548584812848
Felipe Carvalho (Carvalho.fs) <86269606+Carvalhofs@users.noreply.github.com>
2022-10-04 22:44:58 -0300

    linhas

--- pages/Contatos_Especialistas.py
+++ pages/Contatos_Especialistas.py
@@ -2,15 +2,15 @@ import streamlit as st
 import streamlit_modal as modal
 import streamlit.components.v1 as components
 
-st.write("Contatos de especialistas")
+st.write("### Contatos de especialistas\n \n \n")
 
 st.write("https://www.boaconsulta.com/especialistas/diabetes")
 st.write("https://www.doctoralia.com.br/doencas/diabetes-tipo-2")
 st.write("https://www.rodrigobomeny.com.br/tratamentos/diabetes.html")
-st.write("https://lucasmoura.com.br/tratamentos/diabetes")
+st.write("https://lucasmoura.com.br/tratamentos/diabetes\n \n \n")
 
 
-st.write("Mais informações:")
+st.write("### Mais informações:\n \n \n")
 
 st.write("Dráuzio Varella")
 st.write("https://www.drauziovarella.uol.com.br/doencas-e-sintomas/diabetes/")

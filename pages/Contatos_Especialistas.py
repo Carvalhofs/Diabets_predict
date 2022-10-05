83c35f154dfbeeaec1ce92e5a1a8c0fa7ed578be
Felipe Carvalho (Carvalho.fs) <86269606+Carvalhofs@users.noreply.github.com>
2022-10-04 22:37:57 -0300

    www

--- pages/Contatos_Especialistas.py
+++ pages/Contatos_Especialistas.py
@@ -2,5 +2,5 @@ import streamlit as st
 import streamlit_modal as modal
 import streamlit.components.v1 as components
 
-components.iframe("http://drauziovarella.uol.com.br/doencas-e-sintomas/diabetes/")
-components.iframe("http://www.gov.br/saude/pt-br/assuntos/saude-de-a-a-z/d/diabetes")
+components.iframe("www.drauziovarella.uol.com.br/doencas-e-sintomas/diabetes/")
+components.iframe("www.gov.br/saude/pt-br/assuntos/saude-de-a-a-z/d/diabetes")

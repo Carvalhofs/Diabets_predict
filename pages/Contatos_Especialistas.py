fd4670c1560ba0e41935038c4caae6bb0adf0cfb
Felipe Carvalho (Carvalho.fs) <86269606+Carvalhofs@users.noreply.github.com>
2022-10-04 22:34:10 -0300

    espec

---
+++ pages/Contatos_Especialistas.py
@@ -0,0 +1,5 @@
+import streamlit as st
+import streamlit_modal as modal
+import streamlit.components.v1 as components
+
+components.iframe("https://drauziovarella.uol.com.br/doencas-e-sintomas/diabetes/",width=683, height=384)

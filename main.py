923a9690537f09b139f2c870793c42b894e266f0
Felipe Carvalho (Carvalho.fs) <86269606+Carvalhofs@users.noreply.github.com>
2022-10-04 21:19:09 -0300

    tabs

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 import streamlit as st
 import streamlit_modal as modal
 import streamlit.components.v1 as components
+from st_on_hover_tabs import on_hover_tabs
+
+with st.sidebar:
+        tabs = on_hover_tabs(tabName=['Dashboard', 'Money', 'Economy'], 
+                             iconName=['dashboard', 'money', 'economy'],
+                             styles = {'navtab': {'background-color':'#111',
+                                                  'color': '#818181',
+                                                  'font-size': '18px',
+                                                  'transition': '.3s',
+                                                  'white-space': 'nowrap',
+                                                  'text-transform': 'uppercase'},
+                                       'tabOptionsStyle': {':hover :hover': {'color': 'red',
+                                                                      'cursor': 'pointer'}},
+                                       'iconStyle':{'position':'fixed',
+                                                    'left':'7.5px',
+                                                    'text-align': 'left'},
+                                       'tabStyle' : {'list-style-type': 'none',
+                                                     'margin-bottom': '30px',
+                                                     'padding-left': '30px'}},
+                             key="1")
 
 st.title("Diabets prediction")
 st.write("""#### Aplicação busca proporcionar autodiagnóstico preliminar do usuário visando a procura imediata de um médico para diagnóstico precoce de Diabetes.""")
@@ -11,5 +31,5 @@ if open_modal:
 
 if modal.is_open():
     with modal.container():
-        components.iframe('https://carvalhofs-diabets-predict-model-if4iuc.streamlitapp.com/', width = 600, height = 600) 
+        components.iframe('https://carvalhofs-diabets-predict-model-if4iuc.streamlitapp.com/', width = size, height = 600) 
         

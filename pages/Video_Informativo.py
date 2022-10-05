d84f6ef9c1d468cc06ce5a5e55e9a228d311ff56
Felipe Carvalho (Carvalho.fs) <86269606+Carvalhofs@users.noreply.github.com>
2022-10-04 22:02:42 -0300

    fullscre

--- pages/Video_Informativo.py
+++ pages/Video_Informativo.py
@@ -2,4 +2,4 @@ import streamlit as st
 import streamlit_modal as modal
 import streamlit.components.v1 as components
 
-components.iframe("https://www.youtube.com/embed/aErKsc2D8qQ",width=683, height=384, title="Você sabe o que é Diabetes?",allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture", allowfullscreen)
+components.iframe("https://www.youtube.com/embed/aErKsc2D8qQ",width=683, height=384, title="Você sabe o que é Diabetes?",allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"; "fullscreen")

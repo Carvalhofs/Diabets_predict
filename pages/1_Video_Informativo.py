6eb2824e8315cedb8f22f974510fdabf048db50c
Felipe Carvalho (Carvalho.fs) <86269606+Carvalhofs@users.noreply.github.com>
2022-10-04 23:11:43 -0300

    princ

--- pages/1_Video_Informativo.py
+++ pages/1_Video_Informativo.py
@@ -4,3 +4,4 @@ import streamlit.components.v1 as components
 
 components.iframe("https://www.youtube.com/embed/aErKsc2D8qQ",width=683, height=384)
 
+st.markdown("""<p>Retornar para página <a href = "https://carvalhofs-diabets-predict-principal-l1e0kt.streamlitapp.com/", target = "_self">Principal</a></p>""", unsafe_allow_html=True)

9dc724aa2f2079f28ae749fd3c9b7fd8de137786
Felipe Carvalho (Carvalho.fs) <86269606+Carvalhofs@users.noreply.github.com>
2022-10-04 21:41:46 -0300

    video

---
+++ Principal.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import streamlit_modal as modal
+import streamlit.components.v1 as components
+from st_on_hover_tabs import on_hover_tabs
+
+st.title("Diagnóstico preliminar - Diabetes")
+st.write("""#### Clique no botão abaixo para preencher informações necessárias para o diagnóstico.""")
+
+open_modal = st.button("Realizar teste preliminar agora!")
+if open_modal:
+    modal.open()
+
+if modal.is_open():
+    with modal.container():
+        components.iframe('https://carvalhofs-diabets-predict-model-if4iuc.streamlitapp.com/', width = size, height = 600) 
+        
---
+++ pages/Video_Informativo.py
@@ -0,0 +1,5 @@
+import streamlit as st
+import streamlit_modal as modal
+import streamlit.components.v1 as components
+
+components.iframe("https://www.youtube.com/embed/aErKsc2D8qQ")

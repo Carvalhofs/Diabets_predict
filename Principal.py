beb67eb3c0184a193063022fe51b696c4b07c112
Felipe Carvalho (Carvalho.fs) <86269606+Carvalhofs@users.noreply.github.com>
2022-10-04 22:47:21 -0300

    realizar

--- Principal.py
+++ Principal.py
@@ -6,4 +6,4 @@ from st_on_hover_tabs import on_hover_tabs
 st.title("Diagnóstico preliminar - Diabetes")
 st.write("""#### Clique no botão abaixo para preencher informações necessárias para o diagnóstico.""")
 
-st.markdown('<a href="https://carvalhofs-diabets-predict-model-model-hi8fmn.streamlitapp.com/" target="_self"><button type="button">Relizar teste agora!</button></a>', unsafe_allow_html=True)
+st.markdown('<a href="https://carvalhofs-diabets-predict-model-model-hi8fmn.streamlitapp.com/" target="_self"><button type="button">Realizar teste agora!</button></a>', unsafe_allow_html=True)
